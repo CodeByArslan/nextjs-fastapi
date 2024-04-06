11b8202505e35240338afe5c6cf12bb2ee5b08c4
Arslan Rana <rajputarslan693@gmail.com>
2024-04-06 23:51:33 +0500

    done

--- api/index.py
+++ api/index.py
@@ -1,7 +1,43 @@
+
 from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine
+from pydantic import BaseModel
+
+DATABASE_URL = "postgresql://fastnextdb_owner:S82uNJXbBTLW@ep-sparkling-lake-a15kgyar-pooler.ap-southeast-1.aws.neon.tech/fastnextdb?sslmode=require"
+
+engine = create_engine(DATABASE_URL)
+
+def create_table():
+    SQLModel.metadata.create_all(engine)
+
+# Model Schema
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    email: str
+    is_complete: bool = False
+
+# Pydantic validation
+class User_Data(BaseModel):
+    name: str
+    email: str
+    is_complete: bool = False
+
+
+def create_user(name: str, email: str):
+    with Session(engine) as session:
+        user = User(name=name, email=email)
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
+    
+
+create_table() 
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+@app.post("/api")
+def create_user_endpoint(user_data: User_Data):
+    user = create_user(user_data.name, user_data.email)
+    return user
---
+++ components/FormN.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { useState } from 'react';
+
+ function FormN() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = async () => {
+        
+
+        const response = await fetch('/api', {
+
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email })
+        });
+
+        if (response.ok) {
+            console.log('User created successfully');
+            // Optionally, you can redirect the user to another page
+        } else {
+            console.error('Failed to create user');
+        }
+    };
+
+    function showAlert() {
+        alert("Submitted successfully");
+      }
+      
+
+    return (
+
+
+
+
+        
+
+
+
+        
+
+<form className="max-w-sm mx-auto mt-32" onSubmit={handleSubmit}>
+    
+     <div className="mb-5">
+    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" onChange={(e) => setName(e.target.value)} required />
+  </div>
+  <div className="mb-5">
+    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+    <input type="email" id='email'  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" onChange={(e) => setEmail(e.target.value)}
+     required />
+  </div>
+ 
+  <div className="flex items-start mb-5">
+    <div className="flex items-center h-5">
+      <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+       
+       required />
+    </div>
+    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+  </div>
+  <button onClick={showAlert} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+  </button>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+       
+    );
+}
+
+export default FormN;
